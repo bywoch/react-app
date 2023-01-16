e79356fe518c3c3042b4d4345e4abf0acd1ac2b8
BRIGHT.CHUNG <bywoch@gmail.com>
2023-01-16 17:32:54 +0900

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import './App.css';
 /* 각각의 코드가 이름을 가지고 있기에 어떠한 취지의 코드인지도 금방 파악 가능 */
 /* 이렇게 컴퍼넌트 기술을 만드는 React 덕분에 여러 태그들을 하나의 독립 된 부품을 만들수 있고 그 부품들을 이용하면 더 적은 복잡도로 소프트웨어를 만들수 있게 된다 동시에 컴퍼넌트를 공유가 가능하다 / 생산성을 획기적을 끌어올린다 */
 
-function Header() {
+function Header(props) {
+  console.log('props', props, props.title)
   return <header>
-    <h1><a href="/">React</a></h1>
+    <h1><a href="/">{props.title}</a></h1>
   </header>
+  /* 중괄호 사이에 있는 문자는 일반적인 문자열이 아니라 표현식으로 취급 되기 때문에 해석되어 반영이 된다 */
 }
+
 
 function Nav() {
   return <nav>
@@ -23,19 +26,20 @@ function Nav() {
   </nav>
 }
 
-function Article() {
+function Article(props) {
   return <article>
-    <h2>Welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header title="REACT"></Header>
       <Nav></Nav>
-      <Article></Article>
+      <Article title="Welcome" body="Hello, WEB"></Article>
+      <Article title="Hi" body="Hello, React"></Article>
     </div>
   );
 }
